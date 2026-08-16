97e404c67c89766a461a92301efdbbb7556d2782
MrLost <MrLost@DESKTOP-A64DPCU>
2026-08-17 01:28:35 +0330

    Add Swagger support and update launch settings
    Added Swashbuckle.AspNetCore for Swagger integration in Catalog.Api.csproj. Configured Swagger services and UI in Program.cs for API documentation in development. Updated launchSettings.json to auto-launch browser, set app URL to http://localhost:9000, and default to Swagger UI.

--- Services/Catalog.Api/Program.cs
+++ Services/Catalog.Api/Program.cs
@@ -3,6 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Add Swagger Services
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo()
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "Catalog API for the eShopOnContainers project",
+    });     
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -12,6 +26,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
